2fc0bfe3fae34bf86d4480a6a3dd94ab66069163
unknown <24abdullahalmasum@gmail.com>
2023-08-16 13:12:42 +0600

    adding some changes

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'left' });
 
 // typed js
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Backend Developer', 'Traveler'],
+    strings: ['FRONTEND DEVELOPER', 'BACKEND DEVELOPER', 'MERN STACK DEVELOPER'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
